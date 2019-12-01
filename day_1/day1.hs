9f642bc797bb0603a2344f3d66f011d92b505e86
Daniel <Danielsvens@hotmail.com>
2019-12-02 00:22:46 +0100

    adda part2 haskell

--- day_1/day1.hs
+++ day_1/day1.hs
@@ -1,11 +1,19 @@
--- part 1
-
 readInt::String -> Int
 readInt = read
 
 calcFuel::Int -> Int
 calcFuel mass = mass `div` 3 - 2
 
+calcFuelRec::Int -> Int
+calcFuelRec mass
+    | mass < 0 = 0
+    | otherwise = mass + calcFuelRec (calcFuel mass)
+
 main = do
     massList <- readFile "input.txt"
+
+    -- part 1
     print . sum . map calcFuel . map readInt . words $ massList
+    
+    -- part 2
+    print(sum([calcFuelRec y - y | y <- map readInt . words $ massList]))
