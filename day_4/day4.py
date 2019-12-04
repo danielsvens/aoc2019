46baaff81801681edec628e785e5d36a0716f25d
Daniel <Danielsvens@hotmail.com>
2019-12-04 22:59:51 +0100

    adda part2 för pw counter

--- day_4/day4.py
+++ day_4/day4.py
@@ -1,11 +1,28 @@
+from collections import Counter
+
 input0, input1 = '372304', '847060'
-passwords = 0
 
-for num in range(int(input0), int(input1)):
-    num = str(num)
-    if list(sorted(num)) == list(num):
-        if len(list(set(num))) != len(list(num)):
-            passwords = passwords + 1
 
-# Part 1
-print(passwords)
+def password_counter(in0, in1):
+    passwords1, passwords2 = 0, 0
+    for num in range(int(in0), int(in1)):
+        num = str(num)
+
+        # we only accept numbers sorted.
+        if list(sorted(num)) == list(num):
+
+            # now we know there is doubles in there
+            if len(list(set(num))) != len(list(num)):
+                passwords1 = passwords1 + 1
+
+                c = Counter(num)
+                if any([(k, v) for k, v in [(k, v) for k, v in c.items() if v > 1] if v == 2]):
+                    passwords2 = passwords2 + 1
+
+    return passwords1, passwords2
+
+
+# Part 1 / part2
+print(password_counter(input0, input1))
+
+
