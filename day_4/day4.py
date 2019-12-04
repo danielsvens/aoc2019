8aba7199f83f383687a2a41618762e3adeecca29
Daniel <Danielsvens@hotmail.com>
2019-12-05 00:01:59 +0100

    cleanup

--- day_4/day4.py
+++ day_4/day4.py
@@ -1,11 +1,11 @@
 from collections import Counter
 
-input0, input1 = '372304', '847060'
+input0, input1 = 372304, 847060
 
 
 def password_counter(in0, in1):
     passwords1, passwords2 = 0, 0
-    for num in range(int(in0), int(in1)):
+    for num in range(in0, in1):
         num = str(num)
         if list(sorted(num)) == list(num):
             if len(list(set(num))) != len(list(num)):
